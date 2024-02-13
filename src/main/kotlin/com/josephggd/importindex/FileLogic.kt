4e4d333e1a2199d743636fd31805efa3d6ad61a9
Joseph G Gonzalez Diaz <joseph.gonzalez_diaz@us.af.mil>
2024-02-12 22:16:00 -0600

    unused

--- src/main/kotlin/com/josephggd/importindex/FileLogic.kt
+++ src/main/kotlin/com/josephggd/importindex/FileLogic.kt
@@ -12,27 +12,6 @@ import com.intellij.psi.search.GlobalSearchScope
 internal open class FileLogic(val project: Project) {
     private val JAVA = "java"
 
-    fun getFileLevelImports(virtualFile:VirtualFile?):List<PsiJavaFile>{
-        val fileLevelImports = mutableListOf<PsiJavaFile>()
-        if (virtualFile!=null) {
-            val psiFile = PsiManager.getInstance(project).findFile(virtualFile)
-            if (psiFile is PsiJavaFile) {
-                fileLevelImports.add(psiFile)
-            }
-        }
-        return fileLevelImports
-    }
-    fun getProjectLevelImports():List<PsiJavaFile> {
-        val allImports = mutableListOf<PsiJavaFile>()
-        val virtualFiles =
-            FilenameIndex.getAllFilesByExt(project, JAVA, GlobalSearchScope.projectScope(project))
-        for (vf in virtualFiles) {
-            val projectLevelImports = getFileLevelImports(vf)
-            allImports.addAll(projectLevelImports)
-        }
-        return allImports
-    }
-
     fun getImportsAsMap():Map<String,MutableList<PsiJavaFile>>{
         val map = mutableMapOf<String,MutableList<PsiJavaFile>>()
         val virtualFiles =
